3e0b20203199d649e12bb1037d133c2d328a2ee0
Bright-Crest <stephenzhu2004@163.com>
2023-09-29 19:53:36 +0800

    add CMakeLists

---
+++ apple/main.cpp
@@ -0,0 +1,13 @@
+// recognize an apple
+// main.cpp
+
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace std;
+
+int main()
+{
+}
